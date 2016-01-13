944f54e991a12bd3966b96a48da8849917115d0f
timothyhartanto <hartantotimothy@gmail.com>
2016-01-13 21:23:01 +0700

    Update Launcher.java

--- Launcher.java
+++ Launcher.java
@@ -1,5 +1,6 @@
 package com.example.proto.launcher;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -8,7 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Launch extends AppCompatActivity {
+public class Launch extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,18 +17,29 @@ public class Launch extends AppCompatActivity {
         setContentView(R.layout.activity_launcch);
     }
 
+    public void launchAppTwitter(View v){
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("twitter://user?screen_name="));
+            startActivity(intent);
+
+        }catch (Exception e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://twitter.com/#!/")));
+        }
+    }
 
     public void launchApp(View v){
         //first way
-//        Intent facebookIntent = getOpenFacebookIntent(this);
-//        startActivity(facebookIntent);
+        Intent facebookIntent = getOpenFacebookIntent(this);
+        startActivity(facebookIntent);
 
         //second way
-            //String uri = "https://www.facebook.com/<user_id>";
-        String uri = "fb://facewebmodal/f?href=https://www.facebook.com/<user_id>"; // put the profile id here
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-
-        startActivity(intent);
+            //String uri = "https://www.facebook.com/timmy.hartanto";
+//        String uri = "fb://facewebmodal/f?href=https://www.facebook.com/"; // put the profile id here
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+//
+//        startActivity(intent);
 
         //third way
 //        try {
@@ -50,14 +62,15 @@ public class Launch extends AppCompatActivity {
             context.getPackageManager()
                     .getPackageInfo("com.facebook.katana", 0); //Checks if FB is even installed.
             return new Intent(Intent.ACTION_VIEW,
-                    Uri.parse("fb://profile/<user_id>")); //Trys to make intent with FB's URI
+                    Uri.parse("fb://profile/o")); //Trys to make intent with FB's URI /254175194653125
         } catch (Exception e) {
             return new Intent(Intent.ACTION_VIEW,
-                    Uri.parse("https://www.facebook.com/<user_id>")); //catches and opens a url to the desired page
+                    Uri.parse("fb://profile/"));
+//            return new Intent(Intent.ACTION_VIEW,
+//                    Uri.parse("https://www.facebook.com/")); //catches and opens a url to the desired page
         }
     }
-    
-    
+
     /* make the API call */
 //    new GraphRequest(
 //            AccessToken.getCurrentAccessToken(),
@@ -70,5 +83,4 @@ public class Launch extends AppCompatActivity {
 //        }
 //    }
 //    ).executeAsync();
-
 }
