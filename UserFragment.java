d3ac2cab3601670321664aa0b45f62ecf022734f
timothyhartanto <hartantotimothy@gmail.com>
2016-02-02 22:36:57 +0700

    Create UserFragment.java

---
+++ UserFragment.java
@@ -0,0 +1,22 @@
+package com.example.proto.launcher.fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class UserFragment extends Fragment {
+
+    public UserFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
