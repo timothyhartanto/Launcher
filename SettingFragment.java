d4147e36ab500c0c9ceb2f11b091abc2d503988e
timothyhartanto <hartantotimothy@gmail.com>
2016-02-03 21:36:52 +0700

    Create SettingFragment.java

---
+++ SettingFragment.java
@@ -0,0 +1,9 @@
+package com.example.proto.launcher.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Proto on 1/31/2016.
+ */
+public class SettingFragment extends Fragment {
+}
