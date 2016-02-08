e5c55934b5fb39711e7761851b535579b65de166
timothyhartanto <hartantotimothy@gmail.com>
2016-02-08 22:23:33 +0700

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,202 +1,58 @@
-package com.example.proto.launcher;
+package com.example.proto.launcher.fragment;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.example.proto.launcher.fragment.MultiFragment;
+import com.example.proto.launcher.Login;
+import com.example.proto.launcher.R;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by Proto on 1/29/2016.
- */
-public class Login extends Activity implements View.OnClickListener {
-
-    private EditText user, pass;
-    private Button mSubmit, mRegister;
-
-    // Progress Dialog
-    private ProgressDialog pDialog;
-
-    // JSON parser class
-    JSONParser jsonParser = new JSONParser();
-    private static final String LOGIN_URL = "http://xxx.xxx.xxx.xxx:8080/webservice/login.php";
-
-    //JSON element ids from repsonse of php script:
-    private static final String TAG_SUCCESS = "success";
-    private static final String TAG_MESSAGE = "message";
+public class MultiFragment extends AppCompatActivity {
+    public static String PREF_NAME = "loginFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+        // Restore preferences
+        SharedPreferences sp = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        getPref(PREF_NAME, this);
+        boolean already_logged_in = sp.getBoolean("loggedIn", false);
 
-        setContentView(R.layout.login);
+        if(!already_logged_in){
+            Intent i = new Intent(MultiFragment.this, Login.class);
+            startActivity(i);
+        }
+        if(already_logged_in){
+            String username = sp.getString("userName", null);
+            String password = sp.getString("password", null);
+            Login.AttemptLogin login = new Login().new AttemptLogin();
+            login.execute(username, password);
+        }
 
-        //setup input fields
-        user = (EditText)findViewById(R.id.username);
-        pass = (EditText)findViewById(R.id.password);
+        setContentView(R.layout.multi_fragment);
 
-        //setup buttons
-        mSubmit = (Button)findViewById(R.id.login);
-        mRegister = (Button)findViewById(R.id.register);
-
-        //register listeners
-        mSubmit.setOnClickListener(this);
-        mRegister.setOnClickListener(this);
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction().add(R.id.menuContainer, new MenuFragment()).commit();
+            }
 
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.login:
-                String username = user.getText().toString();
-                String password = pass.getText().toString();
-
-                if(username.contentEquals("") || password.contentEquals("")){
-                    Toast.makeText(Login.this, "Please fill all the forms!", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    new AttemptLogin().execute(username, password);
-                }
-                break;
-            case R.id.register:
-                Intent i = new Intent(this, Register.class);
-                startActivity(i);
-                break;
-
-            default:
-                break;
-        }
+    public static SharedPreferences getPref(String file, Context context){
+        return context.getSharedPreferences(file, Context.MODE_PRIVATE);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-
         // Restore preferences
-        SharedPreferences sp = getSharedPreferences("loginFile", MODE_PRIVATE);
+        SharedPreferences sp = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
         boolean already_logged_in = sp.getBoolean("loggedIn", false);
 
-        if(!already_logged_in){
-            this.finish();
-            Intent intent = new Intent(Intent.ACTION_MAIN);
-            intent.addCategory(Intent.CATEGORY_HOME);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
+        if(already_logged_in){
+
         }
     }
-
-    class AttemptLogin extends AsyncTask<String, String, String> {
-
-        /**
-         * Before starting background thread Show Progress Dialog
-         * */
-        boolean failure = false;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pDialog = new ProgressDialog(Login.this);
-            pDialog.setMessage("Attempting login...");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(true);
-            pDialog.show();
-        }
-
-        @Override
-        protected String doInBackground(String... args) {
-            // TODO Auto-generated method stub
-            // Check for success tag
-            int success;
-//            String username = user.getText().toString();
-//            String password = pass.getText().toString();
-
-            String username = args[0];
-            String password = args[1];
-
-            try {
-                // Building Parameters
-                List<NameValuePair> params = new ArrayList<NameValuePair>();
-                params.add(new BasicNameValuePair("username", username));
-                params.add(new BasicNameValuePair("password", password));
-
-                Log.d("request!", "starting");
-                // getting product details by making HTTP request
-                JSONObject json = jsonParser.makeHttpRequest(
-                        LOGIN_URL, "POST", params);
-                if(json == null)
-                    return null;
-
-                // check your log for json response
-                Log.d("Login attempt", json.toString());
-
-                // json success tag
-                success = json.getInt(TAG_SUCCESS);
-                if (success == 1) {
-                    Log.d("Login Successful!", json.toString());
-                    // save user data
-//                    SharedPreferences sp = PreferenceManager
-//                            .getDefaultSharedPreferences(Login.this);
-                    SharedPreferences.Editor edit = getSharedPreferences("loginFile", MODE_PRIVATE).edit();
-                    edit.putBoolean("loggedIn", true);
-                    edit.commit();
-                    
-                    SharedPreferences.Editor editUser = getSharedPreferences("userName", MODE_PRIVATE).edit();
-                    edit.putString("userName", username);
-                    edit.commit();
-                    
-                    SharedPreferences.Editor editPass = getSharedPreferences("password", MODE_PRIVATE).edit();
-                    edit.putString("password", password);
-                    edit.commit();
-                    
-                    //add another saved prefences to save the logins and kepp trying to connect next time it is logged on
-
-                    //Intent i = new Intent(Login.this, MultiFragment.class);
-                    finish();
-                    //startActivity(i);
-                    return json.getString(TAG_MESSAGE);
-                }else{
-                    Log.d("Login Failure!", json.getString(TAG_MESSAGE));
-                    return json.getString(TAG_MESSAGE);
-
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            return null;
-
-        }
-        /**
-         * After completing background task Dismiss the progress dialog
-         * **/
-        protected void onPostExecute(String file_url) {
-            // dismiss the dialog once product deleted
-            pDialog.dismiss();
-            if (file_url != null){
-                Toast.makeText(Login.this, file_url, Toast.LENGTH_LONG).show();
-            }
-
-        }
-
-    }
-
 }
